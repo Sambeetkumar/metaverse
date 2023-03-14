ef7cb5ee3798cc01dd359c7f95fcda858f959a4a
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-03-14 18:43:32 +0530

    Image optimization and lazy loading

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
+import React, { Suspense } from "react";
 import "./App.css";
-import { Navbar, Footer } from "./components";
-import {
-  About,
-  Explore,
-  Feedback,
-  GetStarted,
-  Hero,
-  Insights,
-  WhatsNew,
-  World,
-} from "./sections";
-import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import Spinner from "./components/Spinner";
+const About = React.lazy(() => import("./sections/About"));
+const Explore = React.lazy(() => import("./sections/Explore"));
+const GetStarted = React.lazy(() => import("./sections/GetStarted"));
+const WhatsNew = React.lazy(() => import("./sections/WhatsNew"));
+const World = React.lazy(() => import("./sections/World"));
+const Insights = React.lazy(() => import("./sections/Insights"));
+const Feedback = React.lazy(() => import("./sections/Feedback"));
+const Footer = React.lazy(() => import("./sections/Footer"));
 function App() {
   return (
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <Hero />
-      <div className="relative">
-        <About />
-        <div className="gradient-03 z-0" />
-        <Explore />
-      </div>
-      <div className="relative">
-        <GetStarted />
-        <div className="gradient-04 z-0" />
-        <WhatsNew />
-      </div>
-      <World />
-      <div className="relative">
-        <Insights />
-        <div className="gradient-04 z-0" />
-        <Feedback />
-      </div>
-      <Footer />
+      <Suspense fallback={<Spinner />}>
+        <div className="relative">
+          <About />
+          <div className="gradient-03 z-0" />
+          <Explore />
+        </div>
+        <div className="relative">
+          <GetStarted />
+          <div className="gradient-04 z-0" />
+          <WhatsNew />
+        </div>
+        <World />
+        <div className="relative">
+          <Insights />
+          <div className="gradient-04 z-0" />
+          <Feedback />
+        </div>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
